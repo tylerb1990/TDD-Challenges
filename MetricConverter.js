2ceda8fa9da84243b9d0beb2fa058b643838b11e
Tyler <tyler.bunker17@gmail.com>
2024-03-06 17:30:11 -0600

    create files for second TDD challenge

---
+++ MetricConverter.js
@@ -0,0 +1,11 @@
+/*
+| =========================================================================================
+| Metric Converter Challenge
+| =========================================================================================
+| 
+| Challenge description:
+| 
+| You are to write a metric to imperial unit converter. The code should only focus on the
+| algorithms, no need to need to worry about a UI.
+|
+*/
